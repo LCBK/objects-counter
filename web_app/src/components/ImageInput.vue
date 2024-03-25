6f14da27ae5699c0a463713cda25c31778aa7757
michalkr52 <michalkr53@gmail.com>
2024-03-25 21:39:03 +0100

    Basic image upload buttons

---
+++ web_app/src/components/ImageInput.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import Button from "primevue/button";
+
+const captureInput = ref<HTMLElement>();
+const uploadInput = ref<HTMLElement>();
+
+function onCaptureClick() {
+    if (!captureInput.value) return;
+    captureInput.value.click();
+}
+
+function onUploadClick() {
+    if (!uploadInput.value) return;
+    uploadInput.value.click();
+}
+</script>
+
+<template>
+    <div class="image-select">
+        <Button label="Capture image" icon="pi pi-camera" @click="onCaptureClick()"></Button>
+        <Button label="Upload image" icon="pi pi-upload" @click="onUploadClick()"></Button>
+        <div class="image-select-inputs">
+            <input type="file" name="image-capture" ref="captureInput" accept="image/*" capture="environment" />
+            <input type="file" name="image-upload" ref="uploadInput" accept="image/*" />
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.image-select {
+    display: flex;
+    margin: 0 auto;
+    flex-direction: column;
+    align-items: center;
+    gap: 30px;
+}
+
+.image-select Button {
+    width: 90%;
+    max-width: 240px;
+    height: 50px; 
+}
+
+.image-select-inputs input {
+    width: 0px;
+    height: 0px;
+    overflow: hidden;
+}
+</style>
+
+<style>
+.image-select .p-button-icon {
+    font-size: 1.25rem;
+}
+</style>
--- web_app/src/main.ts
+++ web_app/src/main.ts
@@ -1,11 +1,14 @@
-import './assets/main.css'
+import "./assets/main.css";
+import "primevue/resources/themes/aura-dark-blue/theme.css";
+import 'primeicons/primeicons.css'
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import App from './App.vue'
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import PrimeVue from "primevue/config";
+import App from "./App.vue";
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(createPinia())
+app.use(createPinia()).use(PrimeVue);
 
-app.mount('#app')
+app.mount('#app');
