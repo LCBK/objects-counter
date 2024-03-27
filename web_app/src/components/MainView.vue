64661403fb28676fe83c2270fd95b2caa88f2fd0
Michał Krause <michalkr53@gmail.com>
2024-03-27 19:57:35 +0100

    UI Prototype (#10)
    Implemented a UI prototype in Vue.js using PrimeVue component library.
    
    Closes #2

---
+++ web_app/src/components/MainView.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import ImageInput from "./ImageInput.vue";
+</script>
+
+<template>
+    <div id="main-view" class="view">
+        <ImageInput />
+    </div>
+</template>
+
+<style scoped>
+#main-view {
+    width: 100vw;
+    height: 100vh;          /* added for compatibility */
+    height: 100dvh;
+    display: flex;
+    flex-direction: column;
+    flex-wrap: nowrap;
+}
+</style>
