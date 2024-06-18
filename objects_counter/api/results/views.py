ee8223a5ac164ea760169f8060fd98b77707d439
Konrad Bryłowski <59873325+kobryl@users.noreply.github.com>
2024-06-18 19:34:52 +0200

    update api doc

--- objects_counter/api/results/views.py
+++ objects_counter/api/results/views.py
@@ -5,6 +5,7 @@ from flask_restx import Namespace, Resource
 
 api = Namespace('results', description='Results related operations')
 
+
 @api.route('/get')
 class GetResults(Resource):
     def get(self) -> typing.Any:
@@ -13,15 +14,16 @@ class GetResults(Resource):
 
 @api.route('/<int:result_id>')
 class GetResult(Resource):
-    @api.doc(params={'id': 'The result ID'})
+    @api.doc(params={'result_id': 'The result ID'})
     def get(self, result_id: int) -> typing.Any:
         sleep(5)
         print(result_id)
         return 'Not implemented', 501
 
+
 @api.route('/<int:result_id>/rate')
 class RateResult(Resource):
-    @api.doc(params={'id': 'The result ID'})
+    @api.doc(params={'result_id': 'The result ID'})
     def patch(self, result_id: int) -> typing.Any:
         print(result_id)
         return 'Not implemented', 501
