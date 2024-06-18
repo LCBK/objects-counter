0157f47add5d552dc708fbcd2abd8e09d3eead47
Konrad Bryłowski <59873325+kobryl@users.noreply.github.com>
2024-06-18 19:36:19 +0200

    actions: add pylint.yml (#20)

--- objects_counter/api/results/views.py
+++ objects_counter/api/results/views.py
@@ -5,21 +5,25 @@ from flask_restx import Namespace, Resource
 
 api = Namespace('results', description='Results related operations')
 
+
 @api.route('/get')
 class GetResults(Resource):
     def get(self) -> typing.Any:
         return 'Not implemented', 501
 
 
-@api.route('/<int:id>')
+@api.route('/<int:result_id>')
 class GetResult(Resource):
-    @api.doc(params={'id': 'The result ID'})
-    def get(self, id: int) -> typing.Any:
+    @api.doc(params={'result_id': 'The result ID'})
+    def get(self, result_id: int) -> typing.Any:
         sleep(5)
+        print(result_id)
         return 'Not implemented', 501
 
-@api.route('/<int:id>/rate')
+
+@api.route('/<int:result_id>/rate')
 class RateResult(Resource):
-    @api.doc(params={'id': 'The result ID'})
-    def patch(self, id: int) -> typing.Any:
+    @api.doc(params={'result_id': 'The result ID'})
+    def patch(self, result_id: int) -> typing.Any:
+        print(result_id)
         return 'Not implemented', 501
