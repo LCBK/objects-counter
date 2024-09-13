0d6b311afdaefe3374ce7b688a1bae7c74f1869d
Alicja Lukaszewicz <alisa.lukaszewicz@gmail.com>
2024-09-13 10:50:01 +0200

    refactor: delete redundant new line

--- objects_counter/consts.py
+++ objects_counter/consts.py
@@ -5,6 +5,5 @@ UPLOAD_FOLDER = 'uploads'
 DB_NAME = 'object_counter.sqlite'
 MAX_DB_STRING_LENGTH = 255
 SAM_CHECKPOINT = os.environ.get('SAM_CHECKPOINT')
-
 LOG_LEVEL = os.environ.get('LOG_LEVEL', 'INFO')
 
