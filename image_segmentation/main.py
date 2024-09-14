eb63e6ed6c2d8b22ad0e2b44dca5c89b80adad09
Alicja Lukaszewicz <alisa.lukaszewicz@gmail.com>
2024-09-14 13:54:06 +0200

    feat: change the way image info is stored

--- image_segmentation/main.py
+++ image_segmentation/main.py
@@ -9,34 +9,39 @@ from image_segmentation.segment_anything_object_counter import SegmentAnythingOb
 
 
 def main():
-    image = cv2.imread("images/trains.png")
+    image = cv2.imread("images/img_1.png")
 
     sam_checkpoint = "C:/Users/Alicja/Desktop/Studia/Projekt inżynierski/sam_vit_h_4b8939.pth"
     segmenter = SegmentAnythingObjectCounter(sam_checkpoint)
 
     image_index = segmenter.add_image(image)
     points = [[300, 60], [600, 60], [400, 350], [400, 580], [350, 750]]
-    segmenter.calculate_image_mask(index=image_index, points=points)
+    segmenter.calculate_image_mask(image_index, points=points)
 
-    result_mask = segmenter.get_image_mask(index=image_index)
+    result_mask = segmenter.get_image_mask(image_index)
     result_mask = np.array(result_mask) * 255
     cv2.imwrite("images/results/result.jpg", result_mask)
 
-    object_count = segmenter.get_object_count(index=image_index)
+    object_count = segmenter.get_object_count(image_index)
     print(f"Number of objects detected: {object_count}")
 
     similarity_model = CosineSimilarity()
 
     object_grouper = ObjectClassifier(segmenter, similarity_model)
 
-    grouped_objects = object_grouper.group_objects_by_similarity(threshold=0.7)
+    object_grouper.group_objects_by_similarity()
     print("Grouped objects:")
 
-    for category_id, objects in grouped_objects.items():
-        indices = [obj[0] for obj in objects]
-        print(f"Category {category_id}: Objects {indices}")
+    # Iterate through the categories of objects
+    for category_id, objects in enumerate(segmenter.images[0].categories):
+        # Extract indices of objects in the current category
+        indices = [obj.index for obj in objects]
+        # Detailed information about each object
+        object_details = [(obj.index, obj.top_left_coord, obj.bottom_right_coord, obj.probability) for obj in objects]
+        print(f"Category {category_id}:")
+        print(f"  Object indices: {indices}")
+        print(f"  Object details: {object_details}")
 
 
 if __name__ == "__main__":
     main()
-
--- image_segmentation/object_classification.py
+++ image_segmentation/object_classification.py
@@ -1,4 +1,5 @@
 # pylint: disable-all
+import os
 from collections import defaultdict
 from typing import List, Tuple
 
@@ -11,6 +12,8 @@ from scipy.spatial import distance
 from torch import nn
 from torchvision import transforms as tr
 from torchvision.models import vit_h_14
+
+from image_segmentation.segment_anything_object_counter import Object
 
 
 class CosineSimilarity:
@@ -35,90 +38,100 @@ class CosineSimilarity:
 
 
 class ObjectClassifier:
-    def __init__(self, segmenter, similarity_model: CosineSimilarity):
+    TEMP_IMAGE_DIR = "images/temp"
+
+    def __init__(self, segmenter, similarity_model: CosineSimilarity, image_index: int = 0):
         self.segmenter = segmenter
         self.similarity_model = similarity_model
-        self.categories: defaultdict[int, List[Tuple[int, float]]] = defaultdict(list)
+        self.image_index = image_index
 
         self.embeddings: List[torch.Tensor] = []
-        self.images_of_objects: List[Image.Image] = []
+        self.analyzed_images = set()
 
-    def compute_color_histogram(self, image: Image.Image, bins: int = 16) -> np.ndarray:
+        if not os.path.exists(self.TEMP_IMAGE_DIR):
+            os.makedirs(self.TEMP_IMAGE_DIR)
+
+    @staticmethod
+    def compute_color_histogram(image: Image.Image, bins: int = 16) -> np.ndarray:
+        """Computes the color histogram of an image."""
         image = image.convert('RGB')
         histogram = []
 
+        # Calculate histogram for each channel
         for channel in range(3):
             channel_hist = cv2.calcHist([np.array(image)], [channel], None, [bins], [0, 256])
             channel_hist = cv2.normalize(channel_hist, channel_hist).flatten()
             histogram.append(channel_hist)
 
-        histogram = np.concatenate(histogram)
-        return histogram
+        return np.concatenate(histogram)
 
-    def compute_color_similarity(self, hist1: np.ndarray, hist2: np.ndarray) -> float:
+    @staticmethod
+    def compute_color_similarity(hist1: np.ndarray, hist2: np.ndarray) -> float:
+        """ Computes the color similarity between two histograms. """
         return 1 - distance.braycurtis(hist1, hist2)
 
-    def crop_objects_from_images(self) -> List[Tuple[int, Image.Image]]:
-        cropped_objects = []
-
-        for _, image_data in enumerate(self.segmenter.images):
-            index = 0
-            if image_data.objects_coords:
-                for bbox in image_data.objects_coords:
-                    cropped_object = self.crop_image(image_data.data, bbox)
-                    cropped_objects.append((index, cropped_object))
-                    self.images_of_objects.append(cropped_object)
-                    index += 1
-        return cropped_objects
-
-    def crop_image(self, image: np.ndarray, bbox: Tuple[Tuple[int, int], Tuple[int, int]]) -> Image.Image:
+    @staticmethod
+    def crop_image(image: np.ndarray, bbox: Tuple[Tuple[int, int], Tuple[int, int]]) -> Image.Image:
+        """Crops a single object from the image using the bounding box."""
         pil_image = Image.fromarray(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-
         (x_min, y_min), (x_max, y_max) = bbox
-        cropped_image = pil_image.crop((x_min, y_min, x_max, y_max))
-        return cropped_image
+        return pil_image.crop((x_min, y_min, x_max, y_max))
 
-    def compute_embeddings_for_cropped_objects(self, ):
+    def load_image_from_temp(self, index: int) -> np.ndarray:
+        """Load an image from the temp directory given its index."""
+        filename = f"object_{index}.jpg"
+        temp_path = os.path.join(self.TEMP_IMAGE_DIR, filename)
+        image = Image.open(temp_path)
+        return image
 
-        for index, object_image in enumerate(self.images_of_objects):
-            temp_path = f"images/temp/object_{index}.jpg"
-            object_image.save(temp_path)
+    def save_cropped_image(self, cropped_image: Image.Image, index: int) -> None:
+        """Saves the cropped object image to the temp directory with a unique filename."""
+        image_path = os.path.join(self.TEMP_IMAGE_DIR, f"object_{index}.jpg")
+        cropped_image.save(image_path)
 
+    def delete_images_from_temp_directory(self) -> None:
+        """Deletes image files from the specified temporary directory."""
+
+        for filename in os.listdir(self.TEMP_IMAGE_DIR):
+            file_path = os.path.join(self.TEMP_IMAGE_DIR, filename)
+            try:
+                os.remove(file_path)
+                print(f"Deleted file: {file_path}")
+            except Exception as e:
+                print(f"Error deleting file {file_path}: {e}")
+
+    def crop_objects_from_images(self) -> None:
+        """Crops objects from the specified image and saves them to the temp directory."""
+        image_data = self.segmenter.images[self.image_index]
+        objects = image_data.categories[0]
+
+        # Check if objects have been found - initially all the objects are stored in first category
+        if not image_data.categories or not image_data.categories[0]:
+            return []
+
+        for obj in objects:
+            bbox = (obj.top_left_coord, obj.bottom_right_coord)
+            cropped_image = self.crop_image(image_data.data, bbox)
+            self.save_cropped_image(cropped_image, obj.index)
+
+    def compute_embeddings_for_cropped_objects(self) -> None:
+        """Computes and stores embeddings for all cropped object images."""
+        for filename in sorted(os.listdir(self.TEMP_IMAGE_DIR)):
+            temp_path = os.path.join(self.TEMP_IMAGE_DIR, filename)
             image_tensor = self.similarity_model.preprocess_image(temp_path)
+
             with torch.no_grad():
                 embedding = self.similarity_model.model(image_tensor).cpu()
+
             self.embeddings.append(embedding)
 
-    def group_objects_by_similarity(self, threshold: float = 0.8, color_weight: float = 0.5) -> defaultdict:
-        self.crop_objects_from_images()
-        self.compute_embeddings_for_cropped_objects()
-        self.assign_categories_based_on_similarity(self.images_of_objects, threshold, color_weight)
-        return self.categories
+    def calculate_combined_similarity(self, index_i: int, index_j: int, color_weight: float) -> float:
+        """Calculate combined similarity between two images based on their indices."""
+        image_i = self.load_image_from_temp(index_i)
+        image_j = self.load_image_from_temp(index_j)
 
-    def assign_categories_based_on_similarity(self, images: List[Image.Image], threshold: float, color_weight: float):
-        num_objects = len(self.embeddings)
-        visited = set()
-        category_id = 0
-
-        for index_i in range(num_objects):
-            if index_i in visited:
-                continue
-
-            self.categories[category_id].append((index_i, 1))
-            visited.add(index_i)
-
-            for index_j in range(index_i + 1, num_objects):
-                combined_similarity = self.calculate_combined_similarity(images, index_i, index_j, color_weight)
-
-                if combined_similarity >= threshold:
-                    self.add_image_to_category(index_j, combined_similarity, category_id, visited)
-
-            category_id += 1
-
-    def calculate_combined_similarity(self, images: List[Image.Image], index_i: int, index_j: int,
-                                      color_weight: float) -> float:
-        hist_i = self.compute_color_histogram(images[index_i])
-        hist_j = self.compute_color_histogram(images[index_j])
+        hist_i = self.compute_color_histogram(image_i)
+        hist_j = self.compute_color_histogram(image_j)
 
         embedding_i = self.embeddings[index_i]
         embedding_j = self.embeddings[index_j]
@@ -131,22 +144,68 @@ class ObjectClassifier:
 
         return combined_similarity
 
-    def add_image_to_category(self, index: int, similarity: float, category_id: int, visited: set):
-        added_to_category = False
+    # TODO: write a better algorithm for assigning categories
+    def assign_categories_based_on_similarity(self, threshold: float, color_weight: float):
+        """Assigns objects to categories based on their similarity and returns the result."""
+        image = self.segmenter.images[self.image_index]
+        objects = image.categories[0]
 
-        for images_in_category in self.categories.values():
-            temp_index = 0  # Potential index of given image in different category
-            for img_index, prev_similarity in images_in_category:
-                if index == img_index:
-                    # If previous similarity was worse, delete from that category; otherwise, do nothing
-                    if prev_similarity < similarity:
-                        images_in_category.pop(temp_index)
+        num_objects = len(self.embeddings)
+        category_id = 0
+
+        categories = []
+
+        for index_i in range(num_objects):
+            if index_i in self.analyzed_images:
+                continue
+
+            new_category = []
+            obj_i = objects[index_i]
+            new_category.append(Object(
+                obj_i.index,
+                obj_i.top_left_coord,
+                obj_i.bottom_right_coord,
+                probability=1.0))
+
+            categories.append(new_category)
+            self.analyzed_images.add(index_i)
+
+            for index_j in range(index_i + 1, num_objects):
+                combined_similarity = self.calculate_combined_similarity(index_i, index_j, color_weight)
+                obj_j = objects[index_j]
+
+                if combined_similarity >= threshold:
+                    self.add_object_to_category(obj_j, combined_similarity, category_id, categories)
+
+            category_id += 1
+
+        self.segmenter.images[self.image_index].categories = categories
+
+    def add_object_to_category(self, new_object: Object, similarity: float, category_id: int, categories: list):
+        """Adds an image to a category or updates its similarity if it's already present in a category."""
+
+        # Iterate through all categories to check if the image exists elsewhere
+        for cat_id, category in enumerate(categories):
+            if cat_id == category_id:
+                continue
+
+            # Look for the image in the current category
+            for i, obj in enumerate(category):
+                if obj.index == new_object.index:
+                    if similarity > obj.probability:
+                        # Remove the image from the previous category if the new similarity is better
+                        category.pop(i)
+                        break
                     else:
-                        added_to_category = True
-                    break
-                temp_index += 1
+                        return
 
-        # New image that was not yet categorized
-        if not added_to_category:
-            self.categories[category_id].append((index, similarity))
-            visited.add(index)
+        # The object was not found in another category or the found object had a lower probability
+        new_object.probability = similarity
+        categories[category_id].append(new_object)
+        self.analyzed_images.add(new_object.index)
+
+    def group_objects_by_similarity(self, threshold: float = 0.7, color_weight: float = 0.7) -> defaultdict:
+        self.crop_objects_from_images()
+        self.compute_embeddings_for_cropped_objects()
+        self.assign_categories_based_on_similarity(threshold, color_weight)
+        # self.delete_images_from_temp_directory()
