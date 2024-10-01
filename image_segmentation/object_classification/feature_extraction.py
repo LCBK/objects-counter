b82dd444c44168b938d19aefb9bd87e96074237e
Alicja Lukaszewicz <alisa.lukaszewicz@gmail.com>
2024-10-01 13:50:43 +0200

    temp commit

--- image_segmentation/object_classification/feature_extraction.py
+++ image_segmentation/object_classification/feature_extraction.py
@@ -1,15 +1,57 @@
+import os
+
 import cv2
 import numpy as np
 import torch
 import torchvision
-from PIL import Image
+from PIL import Image as PILImage
 from scipy.spatial import distance
 from torch import nn
 from torchvision import transforms as tr
 from torchvision.models import vit_h_14
 
+from image_segmentation.constants import TEMP_IMAGE_DIR
+from image_segmentation.utils import crop_element
+from objects_counter.db.dataops.image import get_image_by_id
+from objects_counter.db.models import ImageElement
 
-class CosineSimilarity:
+
+class ImageElementProcessor:
+    def __init__(self, feature_similarity_model):
+        self.feature_similarity_model = feature_similarity_model
+
+
+    def process_image_element(self, element: ImageElement) -> None:
+        """Crops, saves, computes embedding, computes histogram, and deletes element image."""
+        image = get_image_by_id(element.image_id)
+        image_data = np.array(PILImage.open(image.filepath))
+
+        cropped_image = crop_element(image_data, element.top_left, element.bottom_right)
+
+        temp_image_path = os.path.join(TEMP_IMAGE_DIR, f"element_{element.id}.jpg")
+        cropped_image.save(temp_image_path)
+
+        embedding = self._calculate_embedding(temp_image_path)
+        histogram = self._calculate_histogram(temp_image_path)
+
+        os.remove(temp_image_path)
+
+        return embedding, histogram
+
+    def _calculate_embedding(self, image_path: str) -> torch.Tensor:
+        """Calculates the embedding for the image at the given path."""
+        image_tensor = self.feature_similarity_model.preprocess_image(image_path)
+        return self.feature_similarity_model.get_embedding(image_tensor)
+
+    def _calculate_histogram(self, image_path: str) -> np.ndarray:
+        """Calculates the histogram for the image at the given path."""
+        image = PILImage.open(image_path)
+        return ColorSimilarity.compute_color_histogram(image, bins=16)
+
+
+class FeatureSimilarity:
+    """Calculates feature similarities of the object using cosine similarity"""
+
     def __init__(self):
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
         self.model = self.load_model()
@@ -24,7 +66,7 @@ class CosineSimilarity:
 
     def preprocess_image(self, image_path: str) -> torch.Tensor:
         """Preprocesses the image before embedding extraction."""
-        img = Image.open(image_path)
+        img = PILImage.open(image_path)
         transformations = tr.Compose(
             [tr.ToTensor(), tr.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)), tr.Resize((518, 518))])
         img = transformations(img).float().unsqueeze_(0).to(self.device)
@@ -37,20 +79,23 @@ class CosineSimilarity:
         return embedding
 
 
-def compute_color_histogram(image: Image.Image, bins: int = 16) -> np.ndarray:
-    """Computes a color histogram for an image."""
-    image = image.convert('RGB')
-    histogram = []
+class ColorSimilarity:
+    """Calculates color similarities of the object using histograms"""
 
-    # Calculate histogram for each channel
-    for channel in range(3):
-        channel_hist = cv2.calcHist([np.array(image)], [channel], None, [bins], [0, 256])
-        channel_hist = cv2.normalize(channel_hist, channel_hist).flatten()
-        histogram.append(channel_hist)
+    @staticmethod
+    def compute_color_histogram(image: PILImage, bins: int = 16) -> np.ndarray:
+        """Computes a color histogram for an image."""
+        image = image.convert('RGB')
+        histogram = []
 
-    return np.concatenate(histogram)
+        for channel in range(3):
+            channel_hist = cv2.calcHist([np.array(image)], [channel], None, [bins], [0, 256])
+            channel_hist = cv2.normalize(channel_hist, channel_hist).flatten()
+            histogram.append(channel_hist)
 
+        return np.concatenate(histogram)
 
-def compute_color_similarity(hist1: np.ndarray, hist2: np.ndarray) -> float:
-    """Calculates the Bray-Curtis distance between two histograms."""
-    return 1 - distance.braycurtis(hist1, hist2)
+    @staticmethod
+    def compute_color_similarity(hist1: np.ndarray, hist2: np.ndarray) -> float:
+        """Calculates the Bray-Curtis distance between two histograms."""
+        return 1 - distance.braycurtis(hist1, hist2)
