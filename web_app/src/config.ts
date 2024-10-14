d9675d5f20d295c89a7a0a14ea41a1c08d56ee5d
michalkr52 <michalkr53@gmail.com>
2024-10-15 00:30:49 +0200

    Classification label ellipsis

--- web_app/src/config.ts
+++ web_app/src/config.ts
@@ -4,6 +4,10 @@ export const config = {
     serverIsAliveDelay: 1500,           // [ms]
     serverIsAliveTimeout: 5000,         // [ms]
 
+    // Layout, display
+    displayClassifications: true,
+    displayCertainty: false,
+
     // User validation
     minUsernameLength: 4,
     maxUsernameLength: 255,
