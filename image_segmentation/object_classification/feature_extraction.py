e733845b5979d868f594520086e0af7e00fb8ddd
Alicja Lukaszewicz <alisa.lukaszewicz@gmail.com>
2024-11-20 00:04:21 +0100

    Change histogram implementation (#128)
    New histogram implementation has been used to mitigate the problems with
    brightness and subtle changes in pixel colors. Originally we used
    histogram intersection that creates histogram for each pixel value. Now
    the implementation has been changed so that rather than taking each
    pixel value while creating histograms, we assign them to specific color
    bins.
    
    Implementation based on the article:
    https://ieeexplore.ieee.org/document/1663800
    
    ---------
    
    Co-authored-by: aleksanderczerwionka <aleks.czerwionka@gmail.com>

--- image_segmentation/object_classification/feature_extraction.py
+++ image_segmentation/object_classification/feature_extraction.py
@@ -7,7 +7,7 @@ from PIL import Image as PILImage
 from torch import nn
 from torchvision import transforms as tr
 
-from image_segmentation.constants import TEMP_IMAGE_DIR
+from image_segmentation.constants import TEMP_IMAGE_DIR, ISCC_NBS_CENTROIDS_RGB, BW, A, SIGMA
 from image_segmentation.utils import crop_element
 from objects_counter.db.dataops.image import get_image_by_id
 from objects_counter.db.models import ImageElement
@@ -32,7 +32,6 @@ class ImageElementProcessor:
         histogram = self._calculate_histogram(temp_image_path)
 
         os.remove(temp_image_path)
-
         return embedding, histogram
 
     def _calculate_embedding(self, image_path: str) -> torch.Tensor:
@@ -43,7 +42,7 @@ class ImageElementProcessor:
     def _calculate_histogram(self, image_path: str) -> np.ndarray:
         """Calculates the histogram for the image at the given path."""
         image = PILImage.open(image_path)
-        return self.color_similarity_model.compute_color_histogram(image, bins=8)
+        return self.color_similarity_model.get_histogram(image)
 
 
 class FeatureSimilarity:
@@ -55,17 +54,16 @@ class FeatureSimilarity:
 
     def load_model(self) -> nn.Module:
         """Loads a pretrained Vision Transformer model."""
-        #todo: load from disk
+        # todo: load from disk
         model = torch.hub.load('facebookresearch/dinov2', 'dinov2_vitg14_reg_lc')
         model = model.to(self.device)
         return model
 
     def preprocess_image(self, image_path: str) -> torch.Tensor:
         """Preprocesses the image before embedding extraction."""
-        img = PILImage.open(image_path)
-        transformations = tr.Compose(
-            [tr.ToTensor(), tr.Resize((224, 224), tr.InterpolationMode.BICUBIC),
-             tr.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))])
+        img = PILImage.open(image_path).convert('RGB')
+        transformations = tr.Compose([tr.ToTensor(), tr.Resize((224, 224), tr.InterpolationMode.BICUBIC),
+                                      tr.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))])
         img = transformations(img).float().unsqueeze_(0).to(self.device)
         return img
 
@@ -79,22 +77,80 @@ class FeatureSimilarity:
 class ColorSimilarity:
     """Calculates color similarities of the object using histograms"""
 
-    @staticmethod
-    def compute_color_histogram(image: PILImage, bins: int = 16) -> np.ndarray:
-        """Computes a color histogram for an image."""
-        image = image.convert('RGB')
-        mask = np.array([not (np.array(image)[x][y] == np.array([255, 255, 255])).all()
-                         for x in range(image.height)
-                         for y in range(image.width)])
-        mask.resize(image.height, image.width)
-        histogram = cv2.calcHist([np.array(image)], [0, 1, 2], np.array(mask, dtype="uint8"),
-                                 [bins, bins, bins],[0, 256, 0, 256, 0, 256])
-        histogram = cv2.normalize(histogram, histogram, 1.0, 0.0, cv2.NORM_L1)
-
-        return histogram
+    ISCC_NBS_CENTROIDS_LUV = cv2.cvtColor(ISCC_NBS_CENTROIDS_RGB, cv2.COLOR_RGB2Luv).reshape(-1, 3)
 
     @staticmethod
     def compute_color_similarity(hist1: np.ndarray, hist2: np.ndarray) -> float:
-        """Calculates the intersection percentage (where 0 means 0% and 1 means 100%) between two histograms."""
-        #todo: test different comp methods and bins
-        return cv2.compareHist(hist1, hist2, cv2.HISTCMP_INTERSECT)
+        """Computes the RGWHI similarity between two histograms."""
+        raw_score = ColorSimilarity.__calculate_histogram_intersection(hist1, hist2)
+        max_score = ColorSimilarity.__calculate_histogram_intersection(hist1, hist1)
+
+        return ColorSimilarity.__normalize_score(raw_score, max_score)
+
+    @staticmethod
+    def __calculate_histogram_intersection(hist_model: np.ndarray, hist_target: np.ndarray) -> float:
+        """Calculates the weighted intersection score between two histograms."""
+        intersection_score = 0.0
+        num_bins = len(hist_model)
+
+        for i in range(num_bins):
+            for j in range(num_bins):
+                distance = ColorSimilarity.__color_distance(ColorSimilarity.ISCC_NBS_CENTROIDS_LUV[i],
+                                                            ColorSimilarity.ISCC_NBS_CENTROIDS_LUV[j])
+                weight = ColorSimilarity.__weight_function(distance)
+                if weight > 0:
+                    intersection_score += min(hist_model[i], hist_target[j]) * weight
+
+        return intersection_score
+
+    @staticmethod
+    def __color_distance(c1: np.ndarray, c2: np.ndarray) -> float:
+        """Computes the Euclidean distance between two colors in LUV space."""
+        return np.sqrt(np.sum((c1 - c2) ** 2))
+
+    @staticmethod
+    def __weight_function(distance: float) -> float:
+        """Applies a weight based on color distance, using a Gaussian function."""
+        if distance <= BW:
+            return (A / (np.sqrt(2 * np.pi) * SIGMA)) * np.exp(-distance ** 2 / (2 * SIGMA ** 2))
+        return 0.0
+
+    @staticmethod
+    def __normalize_score(raw_score: float, max_score: float) -> float:
+        """Normalizes the raw similarity score to a percentage."""
+        return (raw_score / max_score) if max_score != 0 else 0.0
+
+    @staticmethod
+    def get_histogram(image: PILImage) -> np.ndarray:
+        """Computes a color histogram for an image, ignoring white background pixels."""
+
+        mask = ColorSimilarity.__get_mask(image)
+        image = np.array(image)
+
+        image = image.astype(np.float32) / 255.0
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2Luv)
+
+        histogram = np.zeros(len(ColorSimilarity.ISCC_NBS_CENTROIDS_LUV))
+
+        for i, pixel in enumerate(image.reshape(-1, 3)):
+            if mask.reshape(-1)[i]:
+                closest_index = ColorSimilarity.__find_closest_bin_color(pixel)
+                histogram[closest_index] += 1
+
+        histogram = histogram / np.sum(histogram)
+        return histogram
+
+    @staticmethod
+    def __get_mask(image: PILImage):
+        image = image.convert('RGB')
+        mask = np.array(
+            [not (np.array(image)[x][y] == np.array([255, 255, 255])).all() for x in range(image.height) for y in
+             range(image.width)])
+        mask.resize(image.height, image.width)
+        return mask
+
+    @staticmethod
+    def __find_closest_bin_color(pixel_luv: np.ndarray) -> int:
+        """Finds the index of the closest ISCC-NBS color centroid in LUV space."""
+        distances = np.sqrt(np.sum((ColorSimilarity.ISCC_NBS_CENTROIDS_LUV - pixel_luv) ** 2, axis=1))
+        return np.argmin(distances)
